90343860600654c65b5387a8b6ddda4959f8cf23
Abdul1028 <rasoolas2003@gmail.com>
2024-12-21 04:16:33 +0530

    increase time for vercel server functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,9 +66,9 @@ export default function Home() {
       <footer className="border-t py-6 md:py-0">
         <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Built by{" "}
+            Built by ❤️ with {" "}
             <a href="#" className="font-medium underline underline-offset-4">
-              MPSTME Students
+              Abdul Shaikh
             </a>
             . The source code is available on{" "}
             <a
